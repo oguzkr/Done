f4fd93459a9615822ca8a9bb9a3319fcd985e3ab
Oğuz Karatoruk <oguz.karatoruk@flo.com.tr>
2022-07-11 18:42:42 +0300

    Initial Commit

---
+++ Done/DoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoneApp.swift
+//  Done
+//
+//  Created by Oğuz Karatoruk on 11.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
