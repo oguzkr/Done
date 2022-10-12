67469441dd0400c3a1fcd12f4cb88f88630665f0
Oğuz Karatoruk <oguz.karatoruk@flo.com.tr>
2022-10-12 22:04:55 +0300

    Apple watch app added

---
+++ Done! Watch App/Done_App.swift
@@ -0,0 +1,17 @@
+//
+//  Done_App.swift
+//  Done! Watch App
+//
+//  Created by Oğuz Karatoruk on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct Done__Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
