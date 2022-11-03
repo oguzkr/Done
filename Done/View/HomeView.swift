dc31a6924b32c2b2d0ed287e22efbe85d081d913
Oguz <oguzkaratoruk@yandex.com>
2022-11-03 16:54:55 +0300

    fix

--- Done/View/HomeView.swift
+++ Done/View/HomeView.swift
@@ -6,7 +6,6 @@
 //
 
 import SwiftUI
-//MARK: BUG: ilk eklemeden sonraki eklemelerde surekli ilk taskin titleini aliyor
 
 struct HomeView: View {
     @ObservedObject var taskListVM = TaskListViewModel()
@@ -19,27 +18,29 @@ struct HomeView: View {
     var body: some View {
         NavigationView {
             VStack(alignment: .leading) {
-            if taskListVM.taskCellViewModels.count > 0 || self.presentAddNewItem == true {
-                List {
-                    ForEach(taskListVM.taskCellViewModels) { taskCellVM in
-                        TaskCell(taskCellVM: taskCellVM)
-                            .padding(10)
-                            .listRowSeparator(.hidden)
-                            .listRowBackground(Color.clear)
-                            .background(Color(uiColor: taskCellVM.task.color?.name ?? UIColor(Color("defaultTaskColor"))))
-                            .cornerRadius(5)
-                    }
-                }.listStyle(.plain)
-            }
-            else {
-                List {
-                    EmptyTaskView()
-                        .frame(width: 300, height: 300, alignment: .center)
-                        .onTapGesture {
-                            self.presentAddNewItem.toggle()
+                if taskListVM.taskCellViewModels.count > 0 || self.presentAddNewItem == true {
+                    List {
+                        ForEach(taskListVM.taskCellViewModels) { taskCellVM in
+                            TaskCell(taskCellVM: taskCellVM)
+                                .padding(10)
+                                .listRowSeparator(.hidden)
+                                .listRowBackground(Color.clear)
+                                .background(Color(uiColor: taskCellVM.task.color?.name ?? UIColor(Color("defaultTaskColor"))))
+                                .cornerRadius(5)
                         }
+                    }.listStyle(.plain)
+                }
+                else {
+                    List {
+                        EmptyTaskView()
+                            .frame(width: 300, height: 300, alignment: .center)
+                            .onTapGesture {
+                                self.presentAddNewItem.toggle()
+                            }
                     }
                 }
+                
+                
                 Button(action: {
                     self.presentAddNewItem.toggle()
                 }) {
@@ -50,35 +51,20 @@ struct HomeView: View {
                         Text("addNewTask")
                     }
                 }
-            }
-                
-            Button(action: {
-                self.presentAddNewItem.toggle()
-            }) {
-                HStack {
-                    Image(systemName: "plus.circle.fill")
-                        .resizable()
-                        .frame(width: 20, height: 20)
-                    Text("Add New Task")
+                .padding()
+                .sheet(isPresented: $presentAddNewItem) {
+                    AddTaskPopUp(taskText: "")
                 }
-            }
-            .padding()
-            .sheet(isPresented: $presentAddNewItem) {
-                AddTaskPopUp(taskText: "")
-            }
-                
-            }
-            .sheet(isPresented: $showSignInForm) {
-                LoginView()
             }
             .navigationTitle("Done")
         }
     }
-    }
+}
 
-    struct HomeView_Previews: PreviewProvider {
+
+struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
     }
-    }
+}
 
