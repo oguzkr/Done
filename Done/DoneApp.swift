f99cd5ca64800f5df2cbafc86f5e5a99632bbfda
Oguz <oguzkaratoruk@yandex.com>
2022-11-08 22:13:49 +0300

    isfirstlaunch

--- Done/DoneApp.swift
+++ Done/DoneApp.swift
@@ -9,11 +9,14 @@ import SwiftUI
 import Firebase
 
 class AppDelegate: NSObject, UIApplicationDelegate {
+    var isFirstLaunch = false
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
         FirebaseApp.configure()
         if Auth.auth().currentUser == nil {
             Auth.auth().signInAnonymously()
         }
+        isFirstLaunch = UserDefaults.isFirstLaunch()
         return true
     }
 }
@@ -28,3 +31,16 @@ struct DoneApp: App {
     }
 }
 
+extension UserDefaults {
+    // check for is first launch - only true on first invocation after app install, false on all further invocations
+    // Note: Store this value in AppDelegate if you have multiple places where you are checking for this flag
+    static func isFirstLaunch() -> Bool {
+        let hasBeenLaunchedBeforeFlag = "hasBeenLaunchedBeforeFlag"
+        let isFirstLaunch = !UserDefaults.standard.bool(forKey: hasBeenLaunchedBeforeFlag)
+        if (isFirstLaunch) {
+            UserDefaults.standard.set(true, forKey: hasBeenLaunchedBeforeFlag)
+            UserDefaults.standard.synchronize()
+        }
+        return isFirstLaunch
+    }
+}
